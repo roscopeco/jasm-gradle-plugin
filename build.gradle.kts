dbd5713cffc11cf1542f063e03cb1ba42f7562c4
Ross Bamford <roscopeco@gmail.com>
2022-06-25 14:54:17 +0100

    Upgrade to JASM 0.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 
 dependencies {
     implementation("org.ow2.asm:asm:9.3")
-    implementation("com.roscopeco.jasm:jasm:0.5.0")
+    implementation("com.roscopeco.jasm:jasm:0.6.0")
 }
 
 publishing {
